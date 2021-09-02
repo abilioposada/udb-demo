3d6c6798d80af7444146fce8126435fe1593d891
Balbino Aylagas <baylagas@ortussolutions.com>
2021-09-02 16:26:43 -0600

    added a sumar method to make adding easier

--- Sumar.java
+++ Sumar.java
@@ -1,5 +1,10 @@
 public class Sumar{
-    public static void main () {
-        System.out.print( 4 + 2 );
+    public static void main(String[] args) {
+        System.out.println( 4 + 2 );
+        System.out.println(sumar(5,10));
+    }
+
+    public static int sumar(int num1, int num2){
+        return num1 + num2;
     }
 }
